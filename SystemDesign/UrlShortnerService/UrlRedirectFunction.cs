aeeb753d59a69f5966c7b7d24a4bcd529d011c30
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-04-14 00:06:13 +0530

    Full functionality

--- SystemDesign/UrlShortnerService/UrlRedirectFunction.cs
+++ SystemDesign/UrlShortnerService/UrlRedirectFunction.cs
@@ -1,23 +1,22 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using UrlShortenerService.Models;
 
-namespace UrlShortnerService
+namespace UrlShortenerService;
+
+public static class UrlRedirectFunction
 {
-    public static class UrlRedirectFunction
+    [FunctionName(nameof(UrlRedirectFunction))]
+    public static IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "{url}")] HttpRequest req,
+        [CosmosDB(databaseName: "activity-staging", containerName: "sample", Connection = "CosmosDBConnection", Id = "{url}", PartitionKey = "{url}")] ShortenedUrl document,
+        string url,
+        ILogger log)
     {
-        [FunctionName(nameof(UrlRedirectFunction))]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "{url}")] HttpRequest req,
-            string url,
-            ILogger log)
-        {
-            log.LogInformation("{0} function processed a request for url: {1}.", nameof(UrlRedirectFunction), url);
-
-            return new OkObjectResult(url);
-        }
+        log.LogInformation("{0} function processed a request for url: {1}.", nameof(UrlRedirectFunction), url);
+        return new RedirectResult(document.Value, true);
     }
 }
