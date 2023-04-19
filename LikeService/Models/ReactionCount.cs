2133f2e09815862ec151b6a6b4e295ef6764d7e7
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-04-19 08:12:42 +0530

    Moved APIs to different folder

---
+++ LikeService/Models/ReactionCount.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace LikeService.Models;
+
+public record ReactionCount
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+    public string PostId { get; set; }
+    public string CommentId { get; set; }
+    public LikeType LikeType { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int Count { get; set; } = 0;
+}
