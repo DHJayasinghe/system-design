f969db7ff56a6497c27c122954f0732f61783875
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-04-21 09:03:05 +0530

    Added Integration testing

---
+++ LikeService/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(LikeService.Startup))]
+namespace LikeService
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
