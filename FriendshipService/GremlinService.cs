927871c7b5b0b777fc2dee62f954c2ea9f540525
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-05-01 20:55:43 +0530

    Add Friend function

---
+++ FriendshipService/GremlinService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.WebSockets;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Structure.IO.GraphSON;
+using Microsoft.Extensions.Configuration;
+
+namespace FriendshipService;
+
+public class GremlinService
+{
+    private static GraphTraversalSource g = AnonymousTraversalSource.Traversal();
+    private readonly GremlinServer gremlinServer;
+    private readonly ConnectionPoolSettings connectionPoolSettings = new()
+    {
+        MaxInProcessPerConnection = 10,
+        PoolSize = 30,
+        ReconnectionAttempts = 3,
+        ReconnectionBaseDelay = TimeSpan.FromMilliseconds(500)
+    };
+    public GremlinService(IConfiguration configs)
+    {
+        var Primarykey = configs["CosmosDB:PrimaryKey"];
+        var ContainerLink = configs["CosmosDB:ContainerLink"];
+        var Host = configs["CosmosDB:Host"];
+        var Port = int.Parse(configs["CosmosDB:Port"]);
+        var EnableSSL = bool.Parse(configs["CosmosDB:EnableSSL"]);
+        gremlinServer = new GremlinServer(Host, Port, enableSsl: EnableSSL, username: ContainerLink, password: Primarykey);
+    }
+
+    public GremlinClient CreateClient()
+    {
+        var webSocketConfiguration = new Action<ClientWebSocketOptions>(options =>
+        {
+            options.KeepAliveInterval = TimeSpan.FromSeconds(10);
+        });
+
+        return new GremlinClient(gremlinServer, new GraphSON2MessageSerializer(), connectionPoolSettings, webSocketConfiguration);
+    }
+
+    public GraphTraversalSource G => g;
+}
---
+++ FriendshipService/HttpResponseExtension.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace FriendshipService;
+
+public static class HttpResponseExtension
+{
+    public static HttpResponseData OkResult(this HttpRequestData req, string responseText = "")
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/json; charset=utf-8");
+        if (responseText.Length != 0)
+            response.WriteString(responseText);
+        return response;
+    }
+
+    public static HttpResponseData BadResult(this HttpRequestData req, string responseText = "")
+    {
+        var response = req.CreateResponse(HttpStatusCode.BadRequest);
+        response.Headers.Add("Content-Type", "text/json; charset=utf-8");
+        if (responseText.Length != 0)
+            response.WriteString(responseText);
+        return response;
+    }
+
+    public static HttpResponseData ErrorResult(this HttpRequestData req, string responseText = "")
+    {
+        var response = req.CreateResponse(HttpStatusCode.InternalServerError);
+        response.Headers.Add("Content-Type", "text/json; charset=utf-8");
+        if (responseText.Length != 0)
+            response.WriteString(responseText);
+        return response;
+    }
+
+    public static async Task<HttpResponseData> OkObjectResultAsync<T>(this HttpRequestData req, T responseObject)
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/json; charset=utf-8");
+        await response.WriteAsJsonAsync(responseObject);
+        return response;
+    }
+}
