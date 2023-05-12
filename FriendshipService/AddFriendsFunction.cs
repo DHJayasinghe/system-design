18d0adda1f67b00cee481d520de43e39e57efc89
Dhanka Jayasinghe <DHJayasinghe@users.noreply.github.com>
2023-05-12 08:24:26 +0530

    Added basic Friendship service

---
+++ FriendshipService/AddFriendsFunction.cs
@@ -0,0 +1,57 @@
+using System.IO;
+***REMOVED***
+***REMOVED***
+using Gremlin.Net.Driver;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+***REMOVED***
+using Newtonsoft.Json;
+
+namespace FriendshipService;
+
+public class AddFriendsFunction
+***REMOVED***
+    private readonly ILogger _logger;
+    private readonly GremlinService _gremlinService;
+
+    public AddFriendsFunction(ILoggerFactory loggerFactory, GremlinService gremlinService)
+    ***REMOVED***
+        _gremlinService = gremlinService;
+        _logger = loggerFactory.CreateLogger<GetFriendsFunction>();
+***REMOVED***
+
+    [Function(nameof(AddFriendsFunction))]
+    public async Task<HttpResponseData> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "friends")] HttpRequestData req)
+    ***REMOVED***
+        _logger.LogInformation("C# HTTP trigger function processed a ***REMOVED***0***REMOVED*** request.", nameof(AddFriendsFunction));
+
+        var request = JsonConvert.DeserializeObject<AddFriendRequest>(await new StreamReader(req.Body).ReadToEndAsync());
+
+        var persons = new string[]
+        ***REMOVED***
+            string.Format(_gremlinService.UpsertPersonQuery,request.UserId,request.UserName,request.UserEmail),
+            string.Format(_gremlinService.UpsertPersonQuery,request.FriendId,request.FriendName,request.FriendEmail)
+    ***REMOVED***;
+        var friendship = string.Format(_gremlinService.AddFriendshipQuery, request.UserId, request.FriendId);
+
+        using (var gremlinClient = _gremlinService.CreateClient())
+        ***REMOVED***
+            var requests = persons.Select(query => gremlinClient.SubmitAsync<dynamic>(query));
+            await Task.WhenAll(requests);
+            await gremlinClient.SubmitAsync<dynamic>(friendship);
+    ***REMOVED***
+
+        return req.OkResult();
+***REMOVED***
+***REMOVED***
+
+internal record AddFriendRequest
+***REMOVED***
+    public string UserId ***REMOVED*** get; init; ***REMOVED***
+    public string UserEmail ***REMOVED*** get; init; ***REMOVED***
+    public string UserName ***REMOVED*** get; init; ***REMOVED***
+    public string FriendId ***REMOVED*** get; init; ***REMOVED***
+    public string FriendName ***REMOVED*** get; init; ***REMOVED***
+    public string FriendEmail ***REMOVED*** get; init; ***REMOVED***
+***REMOVED***
---
+++ FriendshipService/Program.cs
@@ -0,0 +1,20 @@
+﻿***REMOVED***
+using Microsoft.Extensions.Hosting;
+***REMOVED***
+
+namespace FriendshipService;
+
+public class Program
+***REMOVED***
+    static async Task Main(string[] args)
+    ***REMOVED***
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults((builderContext, builder) =>
+            ***REMOVED***
+        ***REMOVED***.Services.AddSingleton<GremlinService>();
+        ***REMOVED***)
+            .Build();
+
+        await host.RunAsync();
+***REMOVED***
+***REMOVED***
